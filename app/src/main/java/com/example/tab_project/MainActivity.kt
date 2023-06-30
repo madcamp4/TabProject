686d16507303bae0f227c50461dd9027e6c03e4a
lotuxsoo <tndus5383@gmail.com>
2023-06-30 15:27:36 +0900

    first commit

---
+++ app/src/main/java/com/example/tab_project/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.tab_project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.viewpager2.widget.ViewPager2
+import com.example.tab_project.databinding.ActivityMainBinding
+import com.example.tab_project.ui.theme.Tab_projectTheme
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    private val tabTitleArray = arrayOf("First Tab", "Second Tab", "Third Tab")
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+            /**  [TabLayoutMediator]: [TabLayout]과 [ViewPager2]를 연동을 도와주는 객체이다. */
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = tabTitleArray[position]
+            }.attach()
+        }
+    }
+}
