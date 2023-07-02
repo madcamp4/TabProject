73e0cb3de3829470232f50998d1fce8073db52a8
lotuxsoo <https://github.com/lotuxsoo>
2023-07-03 00:33:28 +0900

    gridview

--- app/src/main/java/com/example/tab_project/Fragment02.kt
+++ app/src/main/java/com/example/tab_project/Fragment02.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import androidx.fragment.app.Fragment
-import com.example.tab_project.databinding.Fragment01Binding
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.tab_project.databinding.Fragment02Binding
+
 
 class Fragment02 : Fragment() {
 
@@ -18,6 +20,34 @@ class Fragment02 : Fragment() {
     ): View? {
         binding = Fragment02Binding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val profileAdapter = context?.let { ProfileAdapter(it) }
+        binding.rvProfile.adapter = profileAdapter
+        // binding.rvProfile.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        binding.rvProfile.layoutManager = GridLayoutManager(activity, 2)
+
+        profileAdapter?.datas?.apply {
+            add(ProfileData(img = R.drawable.img_1_right))
+            add(ProfileData(img = R.drawable.img_2_right))
+            add(ProfileData(img = R.drawable.img_3_right))
+            add(ProfileData(img = R.drawable.img_4_right))
+            add(ProfileData(img = R.drawable.img_5_right))
+            add(ProfileData(img = R.drawable.img_1_right))
+            add(ProfileData(img = R.drawable.img_2_right))
+            add(ProfileData(img = R.drawable.img_3_right))
+            add(ProfileData(img = R.drawable.img_4_right))
+            add(ProfileData(img = R.drawable.img_5_right))
+            add(ProfileData(img = R.drawable.img_1_right))
+            add(ProfileData(img = R.drawable.img_2_right))
+            add(ProfileData(img = R.drawable.img_3_right))
+            add(ProfileData(img = R.drawable.img_4_right))
+            add(ProfileData(img = R.drawable.img_5_right))
+            add(ProfileData(img = R.drawable.img_1_right))
+        }
     }
 
 }
--- app/src/main/java/com/example/tab_project/ProfileData.kt
+++ app/src/main/java/com/example/tab_project/ProfileData.kt
@@ -1,7 +1,5 @@
 package com.example.tab_project
 
 data class ProfileData (
-    val name : String,
-    val age : Int,
     val img : Int
 )
