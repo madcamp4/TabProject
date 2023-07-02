13fb76cd7a29718d030374c0c4d64a225dbbfea8
lotuxsoo <tndus5383@gmail.com>
2023-07-03 00:33:28 +0900

    gridview

--- app/src/main/java/com/example/tab_project/ProfileAdapter.kt
+++ app/src/main/java/com/example/tab_project/ProfileAdapter.kt
@@ -5,37 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 class ProfileAdapter(private val context: Context) : RecyclerView.Adapter<ProfileAdapter.ViewHolder>() {
 
-    var datas = mutableListOf<ProfileData>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_recycler_ex,parent,false)
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_ex, parent, false)
+
         return ViewHolder(view)
     }
 
+    var datas = mutableListOf<ProfileData>()
+
     override fun getItemCount(): Int = datas.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ProfileAdapter.ViewHolder, position: Int) {
         holder.bind(datas[position])
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        private val txtName: TextView = itemView.findViewById(R.id.tv_rv_name)
-        private val txtAge: TextView = itemView.findViewById(R.id.tv_rv_age)
         private val imgProfile: ImageView = itemView.findViewById(R.id.img_rv_photo)
 
         fun bind(item: ProfileData) {
-            txtName.text = item.name
-            txtAge.text = item.age.toString()
+
             Glide.with(itemView).load(item.img).into(imgProfile)
 
         }
     }
-
-
 }
