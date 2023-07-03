4f35d19afdce0108610488b981cdeeafd3c6607a
lotuxsoo <tndus5383@gmail.com>
2023-07-04 00:06:59 +0900

    commit

--- app/src/main/java/com/example/tab_project/Fragment02.kt
+++ app/src/main/java/com/example/tab_project/Fragment02.kt
@@ -1,19 +1,22 @@
 package com.example.tab_project
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.BaseAdapter
 import android.widget.GridView
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.tab_project.databinding.Fragment02Binding
-import kotlinx.coroutines.NonDisposableHandle.parent
+
 
 class Fragment02 : Fragment() {
 
@@ -28,12 +31,13 @@ class Fragment02 : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //view의 초기값, Adapter세팅
         super.onViewCreated(view, savedInstanceState)
 
         val profileAdapter = context?.let { ProfileAdapter(it) }
         binding.rvProfile.adapter = profileAdapter
         // binding.rvProfile.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
-        binding.rvProfile.layoutManager = GridLayoutManager(activity, 2)
+        binding.rvProfile.layoutManager = GridLayoutManager(activity, 3)
 
         profileAdapter?.datas?.apply {
             add(ProfileData(img = R.drawable.img_1_right))
@@ -41,20 +45,9 @@ class Fragment02 : Fragment() {
             add(ProfileData(img = R.drawable.img_3_right))
             add(ProfileData(img = R.drawable.img_4_right))
             add(ProfileData(img = R.drawable.img_5_right))
-            add(ProfileData(img = R.drawable.img_1_right))
-            add(ProfileData(img = R.drawable.img_2_right))
-            add(ProfileData(img = R.drawable.img_3_right))
-            add(ProfileData(img = R.drawable.img_4_right))
-            add(ProfileData(img = R.drawable.img_5_right))
-            add(ProfileData(img = R.drawable.img_1_right))
-            add(ProfileData(img = R.drawable.img_2_right))
-            add(ProfileData(img = R.drawable.img_3_right))
-            add(ProfileData(img = R.drawable.img_4_right))
-            add(ProfileData(img = R.drawable.img_5_right))
-            add(ProfileData(img = R.drawable.img_1_right))
         }
 
-        val img = arrayOf(
+        val imageIds = intArrayOf(
             R.drawable.img_1_right,
             R.drawable.img_2_right,
             R.drawable.img_3_right,
@@ -62,43 +55,15 @@ class Fragment02 : Fragment() {
             R.drawable.img_5_right,
         )
 
-        /* class CustomAdapter(val context: Context, val img:Array<Int>) : BaseAdapter(){
-
-            var layoutInflater =  context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val imageAdapter = ImageAdapter(requireContext(),imageIds)
+        binding.myGridView.adapter = imageAdapter
 
 
-                var view = view
-                if(view==null){
-                   view = layoutInflater.inflate(R.layout.item_recycler_ex,parent,false);
-                }
-                //var imageView = view.findViewById<ImageView>(R.id.img_rv_photo);
-                //imageView.setImageResource(R.drawable.img[position])
-
-                return view!!
-
-            }
-
-            override fun getCount(): Int {
-                return img.size
-            }
-
-            override fun getItem(position: Int): Any {
-                return img[position]
-            }
-
-            override fun getItemId(position: Int): Long {
-                return position.toLong()
-            }
+        binding.myGridView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+            val intent = Intent(requireContext(), MainActivity2::class.java)
+            intent.putExtra("imageId", imageIds[position])
+            startActivity(intent)
         }
-
-        var customAdapter = context?.let { CustomAdapter(it,img) };
-        binding.myGridView.adapter = customAdapter;
-        binding.myGridView.setOnItemClickListener { parent, view, position, id ->
-            //Log.e("name",img[position].toString())
-            Toast.makeText(context,"image clicked!",Toast.LENGTH_LONG).show()
-        }
-        */
     }
-
 }
---
+++ app/src/main/java/com/example/tab_project/ImageAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.tab_project
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+
+class ImageAdapter(private val context: Context, private val imageIds: IntArray) : BaseAdapter() {
+
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+
+//    var layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    override fun getCount(): Int {
+        return imageIds.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return imageIds[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view = convertView;
+        if (view == null) {
+            view = layoutInflater.inflate(R.layout.item_recycler_ex, parent, false);
+        }
+        var imageView = view?.findViewById<ImageView>(R.id.img_rv_photo);
+        imageView?.setImageResource(imageIds[position])
+
+        return view!!
+    }
+}
