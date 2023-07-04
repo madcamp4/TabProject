e2e9870b06f3d0f1fc7f1c19c74123fb2bce2ebb
lotuxsoo <tndus5383@gmail.com>
2023-07-04 14:53:22 +0900

    tab3 start

---
+++ app/src/main/java/com/example/tab_project/DiaryData.kt
@@ -0,0 +1,8 @@
+package com.example.tab_project
+
+class DiaryData(
+    var title: String? = null,
+    var date: String? = null,
+    var content: String? = null,
+    var icon: Int? = null
+)
--- app/src/main/java/com/example/tab_project/Fragment03.kt
+++ app/src/main/java/com/example/tab_project/Fragment03.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tab_project.databinding.Fragment01Binding
 import com.example.tab_project.databinding.Fragment03Binding
 
@@ -20,4 +22,18 @@ class Fragment03 : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //view의 초기값, Adapter세팅
+        super.onViewCreated(view, savedInstanceState)
+
+        var diaryAdapter = context?.let { DiaryAdapter(it) }
+        binding.rvDiary.adapter = diaryAdapter
+        binding.rvDiary.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+
+        if (diaryAdapter != null) {
+            diaryAdapter.DiaryList = mutableListOf<DiaryData>((DiaryData("7/4", "제목", "ㅗㅠㅇ류어ㅗㄹ", R.drawable.img_1_right) ))
+            println("ddf")
+        }
+
+    }
 }
